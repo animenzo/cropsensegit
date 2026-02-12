381ae0da31f7699c7e3467e4788e0a16548ae2b3
animenzo <animenzo941@gmail.com>
2026-02-12 20:12:57 +0530

    pwa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -20,4 +20,4 @@ createRoot(document.getElementById("root")).render(
 );
 
 
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate', // Updates app automatically when new content is available
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'favicon.svg'],
+      devOptions: {
+        enabled: true
+      },
       manifest: {
         name: 'CropSense',
         short_name: 'CropSense',
